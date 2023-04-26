d632198fcb67252cb12658675ff9aa389e55e535
Katherine Fortin <kafortin@student.42quebec.com>
2023-04-26 18:16:56 -0400

    philo loop updated

--- srcs/philo.c
+++ srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: kafortin <kafortin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 18:20:04 by kafortin          #+#    #+#             */
-/*   Updated: 2023/04/26 18:04:41 by kafortin         ###   ########.fr       */
+/*   Updated: 2023/04/26 18:16:36 by kafortin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,17 @@ void	*life_of_a_philo(void *lock)
 	return (NULL);
 }
 
-void	init_philos(t_data *data)
+void	init_philos(t_philo *philo, pthread_mutex_t lock)
 {
 	int	i;
 
 	i = 0;
-	while (data->num_philos > i++)
+	while (philo->data->num_philos > i)
 	{
-		// if ((pthread_mutex_init(&data->fork[i], NULL) == -1))
-		// 	exit_error(FORK_INIT_ERROR);
+		if (pthread_create(&philo[i].th, NULL, &life_of_a_philo,
+				(void *)&lock) != 0)
+			exit_error("Thread error\n");
+		i++;
 	}
 }
 
@@ -44,9 +46,6 @@ void	init_data(int argc, char **argv, t_data *data)
 	data->time_to_sleep = ft_atoi(argv[4]);
 	if (argc == 6)
 		data->num_meals = ft_atoi(argv[5]);
-	// data->fork = malloc(sizeof(pthread_mutex_t) * data->num_philos);
-	// if (!data->fork)
-	// 	exit_error(FORK_CREATION_ERROR);
 }
 
 int	main(int argc, char **argv)
@@ -63,15 +62,8 @@ int	main(int argc, char **argv)
 	philo = malloc(sizeof(t_philo) * 10);
 	init_data(argc, argv, &data);
 	philo->data = &data;
-	// init_philos(&data);
-	printf("I'm about to create the threads!\n");
 	pthread_mutex_init(&lock, NULL);
-	while (i < 10)
-	{
-		if (pthread_create(&philo[i].th, NULL, &life_of_a_philo, (void *)&lock) != 0)
-			exit_error("Thread error\n");
-		i++;
-	}
+	init_philos(philo, lock);
 	i = 0;
 	while (i < 10)
 	{
